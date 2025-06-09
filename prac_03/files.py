5b93104d26a176ada0c69b008af4384316146f7b
ZhongSihan <sihan.zhong@my.jcu.edu.au>
2025-06-09 13:07:15 +0800

    Add file reading and writing examples

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,12 +9,10 @@ name_in_file = in_file.read().strip()
 in_file.close()
 print(f"Hi {name_in_file}!")
 
-
 with open("numbers.txt", "r") as in_file:
     first_number = int(in_file.readline())
     second_number = int(in_file.readline())
     print(first_number + second_number)
-
 
 with open("numbers.txt", "r") as in_file:
     total = 0
