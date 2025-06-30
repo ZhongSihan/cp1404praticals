969154421759344de9cba6a769c763d583b7a9a0
ZhongSihan <sihan.zhong@my.jcu.edu.au>
2025-07-01 00:28:12 +0800

    Add ProgrammingLanguage class with __init__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -13,11 +13,3 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
-
-    def is_dynamic(self):
-        """Return True if the language is dynamically typed."""
-        return self.typing.lower() == "dynamic"
-
-    def __str__(self):
-        """Return a string representation of the programming language."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
