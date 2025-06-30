86a1c042eea1d4a33b96c1072ef8ddd44c914f40
ZhongSihan <sihan.zhong@my.jcu.edu.au>
2025-07-01 00:48:57 +0800

    Add Guitar class with __init__ method

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""guitar.py
+Estimate: 15 minutes
+Actual:
+"""
+
+class Guitar:
+    """Represent a Guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
