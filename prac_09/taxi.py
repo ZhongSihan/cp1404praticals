87e984853134843c302be9eb0f505adb3be13398
ZhongSihan <sihan.zhong@my.jcu.edu.au>
2025-07-23 00:25:44 +0800

    Implemented Taxi class with basic functionality

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -1,30 +1,30 @@
-from prac_09.car import Car
-
-class Taxi(Car):
-    """Specialised version of a Car that includes fare costs."""
-
-    price_per_km = 1.23  # Class variable for price per km, shared by all Taxi instances
+class Taxi:
+    """Represent a Taxi."""
+    price_per_km = 1.23
 
     def __init__(self, name, fuel):
-        """Initialise a Taxi instance, based on parent class Car."""
-        super().__init__(name, fuel)
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
         self.current_fare_distance = 0
 
     def __str__(self):
-        """Return a string like a Car but with current fare distance."""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+        """Return a string representation of the Taxi."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self.odometer}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
-        """Return the price for the taxi trip, rounded to nearest 10c."""
-        fare = self.price_per_km * self.current_fare_distance
-        return round(fare, 1)  # Round to nearest 0.10
-
-    def start_fare(self):
-        """Begin a new fare."""
-        self.current_fare_distance = 0
+        """Return the price for the taxi trip, rounded to the nearest 10c."""
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def drive(self, distance):
-        """Drive like parent Car but calculate fare distance as well."""
-        distance_driven = super().drive(distance)
-        self.current_fare_distance += distance_driven
-        return distance_driven
+        """Drive the taxi for the given distance and calculate fare."""
+        if distance > self.fuel:
+            distance = self.fuel
+        self.fuel -= distance
+        self.odometer += distance
+        self.current_fare_distance += distance
+        return distance
+
+    def start_fare(self):
+        """Start a new fare."""
+        self.current_fare_distance = 0
